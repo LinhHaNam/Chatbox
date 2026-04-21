e54d7cc8c0b43045209d828b52860ff0e8b423a5
linhvt <linhvthn2003@gmail.com>
2026-04-21 11:44:51 +0700

    add dockerfile

--- SimsimiChat/obj/Debug/net8.0/SimsimiChat.AssemblyInfo.cs
+++ SimsimiChat/obj/Debug/net8.0/SimsimiChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimsimiChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e7fb20966e52069366ba5076c564511e0e37232")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimsimiChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimsimiChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
